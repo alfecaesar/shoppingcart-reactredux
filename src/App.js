290f06c00ff574e80c62f8a62b011a9657f0a66e
Alfe Caesar Lagas <alfecaesar@gmail.com>
2018-07-18 22:40:15 +0800

    fixed bugs and issues, fixed checkout, product list and shopping cart, added animation, added checkout form validation, added production files

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import CartSummary from './components/CartSummary';
 import ViewCart from './components/ViewCart';
 import Checkout from './components/Checkout';
 
+
 import {
-  BrowserRouter as Router,
   Link,
   Route,
   Switch,
@@ -18,16 +18,16 @@ export const SHOPPINGCART = [];
 export const PRODUCTS = [
                 {
                     photo: "https://fakeimg.pl/165x165/?text=Image",
-    				name: "Apple Iphone 7 Plus",
-                    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere.",
-                    id: "APPLE-123-4567",
-    				price: 45000
-    			}, {
-                    photo: "https://fakeimg.pl/165x165/?text=Image",
     				name: "Apple Iphone X",
                     description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.",
                     id: "APPLE-123-3216",
     				price: 70000
+    			}, {
+                    photo: "https://fakeimg.pl/165x165/?text=Image",
+    				name: "Apple Iphone 8 Plus",
+                    description: "Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.",
+                    id: "APPLE-123-7531",
+    				price: 63000
     			}, {
                     photo: "https://fakeimg.pl/165x165/?text=Image",
     				name: "Apple Iphone 8",
@@ -36,11 +36,30 @@ export const PRODUCTS = [
     				price: 55000
     			}, {
                     photo: "https://fakeimg.pl/165x165/?text=Image",
+    				name: "Apple Iphone 7 Plus",
+                    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla posuere.",
+                    id: "APPLE-123-4567",
+    				price: 45000
+    			}, {
+                    photo: "https://fakeimg.pl/165x165/?text=Image",
     				name: "Apple Iphone 7",
                     description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.",
                     id: "APPLE-123-1478",
     				price: 25000
+    			}, {
+                    photo: "https://fakeimg.pl/165x165/?text=Image",
+    				name: "Apple Iphone 6s Plus",
+                    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.",
+                    id: "APPLE-123-4689",
+    				price: 30000
+    			}, {
+                    photo: "https://fakeimg.pl/165x165/?text=Image",
+    				name: "Apple Iphone 6s",
+                    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec hendrerit tempor tellus. Donec pretium posuere tellus. Proin quam nisl, tincidunt et, mattis eget.",
+                    id: "APPLE-123-1472",
+    				price: 20000
     			}
+
 ];
 
 
@@ -49,7 +68,8 @@ class App extends Component {
   constructor(props){
       super(props);
       this.state = {
-        shoppingCartCount : 0
+        shoppingCartCount : 0,
+        SHOPPINGCART : []
       }
   } 
     
@@ -60,11 +80,15 @@ class App extends Component {
         
   }
     
-  decrementCount = () => {
+  clearCount = () => {
       this.setState({ 
-          shoppingCartCount: this.shoppingCartCount - 1 
+          shoppingCartCount: 0,
+          SHOPPINGCART: []
       })
   }
+  
+  
+  
     
   render() {
     return (
@@ -72,7 +96,7 @@ class App extends Component {
             <Navbar>
               <Navbar.Header>
                 <Navbar.Brand>
-                  <a href="/">My Shopping Cart</a>
+                  <Link to={{ pathname: '/' }}>My Shopping Cart</Link>
                 </Navbar.Brand>
                 <Navbar.Toggle />
               </Navbar.Header>
@@ -85,17 +109,19 @@ class App extends Component {
             </Navbar>
         
             <Grid>
-                <CartSummary carts={SHOPPINGCART} shoppingCartCount={this.state.shoppingCartCount} />
+                <CartSummary carts={this.state.SHOPPINGCART} shoppingCartCount={this.state.shoppingCartCount} />
             </Grid>
             
 
             <Switch>
+                
+                    <Route exact path="/" render={() => <ProductList shoppingCart={this.state.SHOPPINGCART} addCount={this.incrementCount}  minusCount={this.decrementCount}/>} />
         
-                <Route exact path="/" render={() => <ProductList addCount={this.incrementCount}  minusCount={this.decrementCount}/>} />
-           
-                <Route path="/view-cart" component={ViewCart}  />
+                    <Route path="/view-cart" render={() => <ViewCart clearCount={this.clearCount} shoppingCart={this.state.SHOPPINGCART} />} />
         
-                <Route path="/checkout" component={Checkout} />
+                    <Route path="/checkout" component={Checkout} />
+                
+                
             </Switch>
         </div>
     );
--- src/components/ViewCart.js
+++ src/components/ViewCart.js
@@ -2,25 +2,31 @@ import React, { Component } from 'react';
 import '../App.css';
 import { Grid, Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { PRODUCTS, SHOPPINGCART } from '../App';
+import { PRODUCTS } from '../App';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
 class ViewCart extends Component {
     
+  clearCartClick(a) { 
+    
+    this.totalAmount = 0;
+    this.props.clearCount()
+    
+  }
+    
   render() {
       
     const totalAmountArr = [];  
       
-    const totalAmount = 0;
-      
     const rows = [];
       
-    SHOPPINGCART.forEach((scItem) => {
+    this.props.shoppingCart.forEach((scItem) => {
         PRODUCTS.forEach((product) => {
             if(scItem === product.id){ 
                 
-                totalAmountArr.push(parseInt(product.price));
+                totalAmountArr.push(parseInt(product.price,10)); 
                 
                 this.totalAmount =  totalAmountArr.reduce(reducer); 
                 
@@ -46,40 +52,42 @@ class ViewCart extends Component {
       
     return (
         <Grid>
-            <Row className="show-grid row-border-bottom padding-top-m">
-                <Col xs={4} >
-                    <strong>ID</strong>
-                </Col>
-                <Col xs={5} >
-                    <strong>Name</strong>
-                </Col>
-                <Col xs={3} className="text-right">
-                    <strong>Amount</strong>
-                </Col>
-            </Row>
-            {rows}
-        
-            <Row className="show-grid row-border-top row-border-top">
-                <Col xs={12} className="text-right grandTotalTxt">
-                    <strong>Grand Total</strong>
-                </Col>
-            </Row>
-        
-            <Row className="show-grid row-border-bottom">
-                <Col xs={12} className="text-right grandTotalTxt">
-                    <strong>{this.totalAmount.toLocaleString(navigator.language, { minimumFractionDigits: 2 })}</strong>
-                </Col>
-            </Row>
-        
-            <Row className="show-grid text-center backtohome">
-                <Col xs={12} >
-                    <Link to={{ pathname: '/' }}><Button>Back to Home</Button></Link>
-                        
-                    <Button bsStyle="danger">Clear Cart</Button>
-                        
-                    <Link to={{ pathname: '/checkout' }}><Button bsStyle="success">Checkout</Button></Link>
-                </Col>
-            </Row>
+            <CSSTransitionGroup transitionName="example" transitionAppear={true} transitionAppearTimeout={500} transitionEnter={false} transitionLeave={false}>
+                <Row className="show-grid row-border-bottom padding-top-m">
+                    <Col xs={4} >
+                        <strong>ID</strong>
+                    </Col>
+                    <Col xs={5} >
+                        <strong>Name</strong>
+                    </Col>
+                    <Col xs={3} className="text-right">
+                        <strong>Amount</strong>
+                    </Col>
+                </Row>
+                {rows}
+
+                <Row className="show-grid row-border-top row-border-top">
+                    <Col xs={12} className="text-right grandTotalTxt">
+                        <strong>Grand Total</strong>
+                    </Col>
+                </Row>
+
+                <Row className="show-grid row-border-bottom">
+                    <Col xs={12} className="text-right grandTotalTxt">
+                        <strong>{this.totalAmount.toLocaleString(navigator.language, { minimumFractionDigits: 2 })}</strong>
+                    </Col>
+                </Row>
+
+                <Row className="show-grid text-center backtohome">
+                    <Col xs={12} >
+                        <Link to={{ pathname: '/' }}><Button>Back to Home</Button></Link>
+
+                        <Button bsStyle="danger" onClick={this.clearCartClick.bind(this)}>Clear Cart</Button>
+
+                        <Link to={{ pathname: '/checkout' }}><Button bsStyle="success">Checkout</Button></Link>
+                    </Col>
+                </Row>
+            </CSSTransitionGroup>
         </Grid>
     );
   }
